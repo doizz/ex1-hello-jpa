10a17e8348f3bef793d97b9e91759f03e73d8e67
jung <doizz@naver.com>
2022-04-12 21:38:53 +0900

    영속성 전이(CACADE)와 고아객체

---
+++ src/main/java/hellojpa/Child.java
@@ -0,0 +1,42 @@
+package hellojpa;
+
+import javax.persistence.*;
+
+@Entity
+public class Child {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "MEMBER_ID")
+    private Long id;
+
+    private String username;
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Parent parent;
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -18,16 +18,21 @@ public class JpaMain {
 
         try {
 
-            Member member = new Member();
-            member.setUsername("Hellow");
-            em.persist(member);
+            Child child1 = new Child();
+            Child child2 = new Child();
 
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
+
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            List<Member> members = em.createQuery("select m from Member m ", Member.class)
-                    .getResultList();
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
+
 
 
 
@@ -162,6 +167,46 @@ public class JpaMain {
  *  - 모든 연관관계에세 지연로딩을 사용해라(중요)
  *  - 실무에서 즉시 로딩을 사용하지마라 (중요)
  *  - 즉시로딩은 상상하지 못한 쿼리가 나간다.
+ *
+ *
+ * # 영속성 전이 :CASCADE
+ * - 특정 엔티티를 영속 상태로 만들 때 연관된 엔티티도 함께 영속 상태로 만들고 싶을 때
+ * - 예 ) 부모 엔티티를 저장할 때 자식 엔티티도 함께 저장.
+ *
+ * - 양속성 전이 : CASCADE - 주의!!!!
+ *  - 영속성 전이는 연관관계를 매핑하는 것과 아무 연관이 없음.
+ *  - 엔티티를 영속화할 때 연관된 엔티티도 함께 영속화하는 편리함을 제공할뿐
+ *
+ * - CASCADE의 종류
+ *  - ALL : 모두 적용
+ *  - PERSIST : 영속
+ *  - REMOVE : 삭제
+ *  - MERGE : 병합
+ *  - REFRESH : REFRESH
+ *  - DETACH : DETACH
+ *
+ * # 고아 객체
+ *  - 고아 객체 제거 : 부모 엔티티와 연관관계가 끊어진 자식 엔티티를 자동으로 삭제
+ *  - orphanRemoval = true
+ *  - Parent parent1 = em.find(Parent.class, id);
+ *    parent1.getChildren().remove(0);
+ *    //자식 엔티티를 컬렉션에서 제거
+ * # 고아 객체 - 주의!!!
+ * - 참조가 제거된 엔티티는 다른 곳에서 참조하지 않은 고아 객체로 보고 삭제하는 기능
+ * - 참조하는 곳이 하나일때 사용해야함.
+ * - 특정엔티티가 개인 소유 할 때 사용
+ * - @OneToOne , @OneToMany만 가능
+ * - 참고 : 개념적으로 부모를 제거하면 자식은 고아가 된다 따라서
+ *   고아 객체 제거기능을 활성화 하면, 부모를 제거할 때 자식도 함께 제거된다.
+ *   이것은 CascadeType.REMOVE 처럼 동작한다.
+ *
+ * # 영속성 전이 + 고아객체, 생명주기
+ *  - CascadeType.ALL + orphanRemovel= true
+ *  - 스스로 생명주기를 관리하는 엔티티는 em.persist()로 영속화 , em.remove()로 제거
+ *  - 두 옵션을 모두 활성화 하면 부모 엔티티를 통해 자식의 생명주기를 관리할 수 있음.
+ *  - 도메인 주도 설계의 Aggregate Root 개념을 구현할때 유용
+ *
+ *
  *
  * */
 
