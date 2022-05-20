940fa93aa70cc76927f11a469c03abcadcf4ca53
jung <doizz@naver.com>
2022-05-20 20:40:22 +0900

    값 타입의 비교

--- src/main/java/hellojpa/ValueMain.java
+++ src/main/java/hellojpa/ValueMain.java
@@ -3,12 +3,15 @@ package hellojpa;
 public class ValueMain {
 
     public static void main(String[] args) {
+        //값 타입 비교 예제
 
-        Integer a = new Integer(10);
-        Integer b = a;
+        int a = 10;
+        int b = 10;
+        Address address1 = new Address("city", "street", "10000");
+        Address address2 = new Address("city", "street", "10000");
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
+        System.out.println("(a ==b) = " + (a == b));
+        System.out.println("(address1.equals(addre)) = " + (address1.equals(address2)));
 
     }
 }
